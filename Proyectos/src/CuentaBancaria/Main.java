5d97424bc522863756c38457217a83ce031c768a
mnocea <mnoceacabrera@gmail.com>
2023-01-24 11:55:07 +0100

    subir

--- Proyectos/src/CuentaBancaria/Main.java
+++ Proyectos/src/CuentaBancaria/Main.java
@@ -1,10 +1,16 @@
 package CuentaBancaria;
-
+/*
+ * Parte principal del codigo donde se llaman a los métodos
+ * Acceso a la aplicación de gestión de cuentas.
+ * */
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//Inicializar el tipo cuenta
+		//Creación del menú por consola
+		//Scanner con la opción
+		//Control de casos
+			
 	}
 	
 }
