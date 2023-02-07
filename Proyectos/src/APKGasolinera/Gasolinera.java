43bdd7d17b6dad19bad47c18cfc0f5f81e3739e8
mnoceacabrera@gmail.com <mnoceacabrera@gmail.com>
2023-02-07 19:43:54 +0100

    GasolineraTerminada

--- Proyectos/src/APKGasolinera/Gasolinera.java
+++ Proyectos/src/APKGasolinera/Gasolinera.java
@@ -1,20 +1,21 @@
 package APKGasolinera;
 
 import java.util.List;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Gasolinera {
 	//Atributos
-	private String fecha;
+	private Date fecha;
 	private double litros;
 	private double importe;
 	private String dni;
 	private String matricula;
 	//Getters and Setters
-	public String getFecha() {
+	public Date getFecha() {
 		return fecha;
 	}
-	public void setFecha(String fecha) {
+	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
 	public double getLitros() {
@@ -42,19 +43,68 @@ public class Gasolinera {
 		this.matricula = matricula;
 	}
 	public Gasolinera RepostajeNormal(){
-		Gasolinera nuevo=new Gasolinera();
+		//creo una nueva gasolinera
+		Gasolinera gasolineraVacia=new Gasolinera();
+		Date fechaActual=new Date();
+		//se piden los datos
 		System.out.println("Introduce la cantidad de litros :");
 		Scanner scanLitros=new Scanner(System.in);
 		double litros=scanLitros.nextDouble();
 		System.out.println("Introduce el importe :");
 		Scanner scanImporte=new Scanner(System.in);
 		double importe=scanImporte.nextDouble();
-		//Por aqui----------------------------------------------------------------------
-		String fecha=
-		nuevo.setLitros(litros);
-		nuevo.setImporte(importe);
-		nuevo.setFecha(fecha);
-		return nuevo;
+		//se cambian los datos de la nueva gasolinera
+		gasolineraVacia.setLitros(litros);
+		gasolineraVacia.setImporte(importe);
+		gasolineraVacia.setFecha(fechaActual);
+		//se devuelve la nueva gasolinera
+		return gasolineraVacia;
+	}
+	public Gasolinera RepostajeFactura(){
+		//creo una nueva gasolinera
+		Gasolinera gasolineraVacia=new Gasolinera();
+		Date fechaActual=new Date();
+		//se piden los datos
+		System.out.println("Introduce tu dni: ");
+		Scanner nuevoDni=new Scanner(System.in);
+		String dni=nuevoDni.next();
+		System.out.println("Introduce tu matricula: ");
+		Scanner nuevaMatricula=new Scanner(System.in);
+		String matricula=nuevoDni.next();		
+		System.out.println("Introduce la cantidad de litros :");
+		Scanner scanLitros=new Scanner(System.in);
+		double litros=scanLitros.nextDouble();
+		System.out.println("Introduce el importe :");
+		Scanner scanImporte=new Scanner(System.in);
+		double importe=scanImporte.nextDouble();
+		
+		//se cambian los datos de la nueva gasolinera
+		gasolineraVacia.setLitros(litros);
+		gasolineraVacia.setImporte(importe);
+		gasolineraVacia.setFecha(fechaActual);
+		gasolineraVacia.setDni(dni);
+		gasolineraVacia.setMatricula(matricula);
+		//se devuelve la nueva gasolinera
+		return gasolineraVacia;
+	}
+	public void VerRepostajes(List<Gasolinera> bd) {
+		String dni;
+		String matricula;
+		double importe;
+		double litros;
+		Date fecha;
+		for (int i = 0; i <bd.size(); i++) {
+			dni=bd.get(i).getDni();
+			matricula=bd.get(i).getMatricula();
+			importe=bd.get(i).getImporte();
+			litros=bd.get(i).getLitros();
+			fecha=bd.get(i).getFecha();
+			 if(matricula==null||dni==null) {
+				 System.out.println("Repostaje Normal--> Fecha: "+fecha+"; Importe: "+importe+"; Litros:"+litros);
+			 }
+			 else
+				 System.out.println("Repostaje Factura--> Fecha: "+fecha+"; DNI: "+dni+"; Matricula: "+matricula+"; Importe: "+importe+"; Litros:"+litros);
+		}
 	}
 
 }
--- Proyectos/src/APKGasolinera/MainGasolinera.java
+++ Proyectos/src/APKGasolinera/MainGasolinera.java
@@ -23,22 +23,23 @@ public class MainGasolinera {
 					break;
 				case 2:
 					//repostaje factura
+					repostajes.add(nuevaGasolinera.RepostajeFactura());
 					break;
 				case 3:
 					//ver todos los repostajes
+					nuevaGasolinera.VerRepostajes(repostajes);
 					break;
 				case 4:	
 					//salir de la aplicacion
+					System.out.println("Gracias por usar la gasolinera");
 					break;
 				}
 				}while(opcion!=4);
-				for (int i = 0; i < repostajes.size(); i++) {
-					System.out.println(repostajes.get(i));
-				}
+				
 				
 	}
 	static void MENU() {
-		System.out.println("--------MENÚ--------");
+		System.out.println("--------MENU--------");
 		System.out.println("1-->Repostaje Normal");
 		System.out.println("2-->Repostaje factura");
 		System.out.println("3-->Ver todos los repostajes");
