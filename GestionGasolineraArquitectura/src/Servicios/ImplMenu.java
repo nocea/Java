e2adf5df877636d181073b312d6a8a79712d5216
mnocea <mnoceacabrera@gmail.com>
2023-03-03 10:08:56 +0100

    update

--- GestionGasolineraArquitectura/src/Servicios/ImplMenu.java
+++ GestionGasolineraArquitectura/src/Servicios/ImplMenu.java
@@ -1,6 +1,6 @@
 package Servicios;
 
-public class ImplMenu {
+public class ImplMenu implements InterfazMenu {
 /*
 * Se crea un Impl y una Interfaz por cada uno de los controladores y de entidades que tenga
 */
--- GestionGasolineraArquitectura/src/Servicios/ImplRepostaje.java
+++ GestionGasolineraArquitectura/src/Servicios/ImplRepostaje.java
@@ -1,6 +1,6 @@
 package Servicios;
 
-public class ImplRepostaje {
+public class ImplRepostaje implements InterfazRepostaje {
 	/*
 	* Se crea un Impl y una Interfaz por cada uno de los controladores y de entidades que tenga
 	*/
