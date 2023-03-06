2369c6956527f0e370d03659cd09842e24a5916f
mnocea <mnoceacabrera@gmail.com>
2023-03-06 12:33:35 +0100

    to string

--- GestionGasolineraArquitectura/src/Entidades/Repostaje.java
+++ GestionGasolineraArquitectura/src/Entidades/Repostaje.java
@@ -7,4 +7,11 @@ public class Repostaje {
 Tambien van aqui los constructores (Si creo alguno y quiero usar 
 el vacio lo tengo que crear tambien)
 */
+/*
+ * El toString sirve para listar en la consola ese objeto con todos los atributos(facilita escribirlo en consola).
+	@Override
+	public String toString() {
+		return "Repostaje [id=" + id + ", litros=" + litros + ", importe=" + importe + ", fchRepostaje=" + fchRepostaje
+				+ ", dni=" + dni + ", matricula=" + matricula + "]";
+*/
 }
